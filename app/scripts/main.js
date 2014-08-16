7ca425d1c5b36bee510b2b6652d20498556621b2
seven_ply <seven-ply@web.de>
2014-08-16 18:16:05 +0200

    commit for deployment

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,21 @@
-$('.carousel').carousel({
-  interval: 5000
-})
+$(document).ready(function() {
+	$('.carousel').carousel({
+	  interval: 5000
+	})
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInUp');
+	}, {
+		offset: '100%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '100%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInDown');
+	}, {
+		offset: '100%'
+	});
+});
